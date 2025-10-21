77fcdacd517f08bc8f83d7841426ded71703aa14
DiegoDenzer <diegodenzer@hotmail.com>
2025-10-21 20:38:45 -0300

    Realizando a criação basica de um projeto

---
+++ src/main/java/br/com/s2dtech/sonico/SonicoBpmnApplication.java
@@ -0,0 +1,13 @@
+package br.com.s2dtech.sonico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonicoBpmnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SonicoBpmnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/s2dtech/sonico/SonicoBpmnApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.s2dtech.sonico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonicoBpmnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
